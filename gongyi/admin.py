0b574df1b750085366c5aad8ebc3020ab7ff6fd7
HM <lihuimin@chunyu.me>
2015-07-30 16:47:30 +0800

    update admin

--- gongyi/admin.py
+++ gongyi/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from gongyi.models import XiaoHuaGongYi
 
 # Register your models here.
+
+class XiaoHuaGongYiAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(XiaoHuaGongYi, XiaoHuaGongYiAdmin)
