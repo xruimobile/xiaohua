040b3ce137254f7d244521405f1b8b62b218abe5
HM <lihuimin@chunyu.me>
2015-07-30 16:43:17 +0800

    update model

--- gongyi/models.py
+++ gongyi/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class XiaoHuaGongYi(models.Model):
+    '''
+    '''
+    xiaohua_name = models.CharField(max_length=100, default='xiaohua')
+    money_quota = models.CharField(max_length=20, default='0')
+    money_get = models.CharField(max_length=20, default='0')
+
+    xiaohua_story = models.TextField(default='')
+
+    xiaohua_pic_list = models.TextField(default='')
