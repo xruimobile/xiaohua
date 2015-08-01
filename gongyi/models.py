8ea85a16903f580ba533fcc10e6c02bd3366e4c4
HM <lihuimin@chunyu.me>
2015-08-01 19:54:18 +0800

    test

--- gongyi/models.py
+++ gongyi/models.py
@@ -34,7 +34,7 @@ class Children(models.Model):
         timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
         file_name = str(self.child_id) + '_' + timestamp + '.jpg'
         comm = 'scp /root/workspace/xiaohua/media/' + os.path.basename(self.icon.name) \
-               + ' root@liaomeizhi:/home/liaomeizhi_www/static/images/children/' + file_name
+               + ' root@liaomeizhi.com:/home/liaomeizhi_www/static/images/children/' + file_name
         try:
             out = subprocess.check_output(comm, shell=True)
         except:
