f49032ff01eb0fbc36bc130edeb0ce7905a43564
HM <lihuimin@chunyu.me>
2015-08-02 09:09:53 +0800

    update databases profile

--- xiaohua/settings.py
+++ xiaohua/settings.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 """
 Django settings for xiaohua project.
 
@@ -77,10 +78,20 @@ WSGI_APPLICATION = 'xiaohua.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'xdefault': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    },
+
+    'default': { # mysql
+        'ENGINE': 'django.db.backends.mysql', #设置为mysql数据库
+        'NAME': 'liaomeizhi_db_prod_check',  #mysql数据库名
+        'USER': 'liaomeizhi_api',  #mysql用户名，留空则默认为当前linux用户名
+        'PASSWORD': 'liaomeizhi2013NX',   #mysql密码
+        'HOST': 'liaomeizhi.com',  #留空默认为localhost
+        'PORT': '',  #留空默认为3306端口
+    },
+
 }
 
 
