3c8239299bead40c86c3dcc6eb3736ca767f31e8
HM <lihuimin@chunyu.me>
2015-08-05 17:38:12 +0800

    update model

--- gongyi/models.py
+++ gongyi/models.py
@@ -25,6 +25,8 @@ class Children(models.Model):
     introduce = models.CharField(max_length=255, default='')
 
     sound_id = models.IntegerField(max_length=11, default=0)
+    sound_url = models.CharField(max_length=255, default='')
+    sound_photo_url = models.CharField(max_length=255, default='')
     photo_array = models.CharField(max_length=255, default='')
     status = models.IntegerField(max_length=1, default=0)
 
